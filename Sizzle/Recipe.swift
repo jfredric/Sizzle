6109199ddd80a51b7bbe1dc1e51f938a490eba4f
jfredrickson <jfredric@gmail.com>
2017-11-28 22:44:27 -0800

    - basic table views set up - new (still temporary) appIcon. Fixes alpha issue. - basic recipe model, and tests written

---
+++ Sizzle/Recipe.swift
@@ -0,0 +1,58 @@
+//
+//  Recipe.swift
+//  Sizzle
+//
+//  Created by Joshua Fredrickson on 11/26/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import Foundation
+
+class Recipe {
+    public var title: String
+    private var currentStep: Int?
+    private var steps: [String]
+    
+    init(title: String, steps: [String]) {
+        self.title = title
+        self.steps = steps
+    }
+    
+    // Returns nil if no more steps exist
+    func next() -> String? {
+        //empty list returns nil
+        if steps.count == 0 {
+            return nil
+        }
+        
+        if let current = currentStep {
+            // end of steps, returns nil
+            if current == steps.count {
+                return nil
+            }
+            currentStep = current + 1
+            return steps[current]
+        } else {
+            currentStep = 1
+            return steps[0]
+        }
+        
+    }
+    
+    
+    // test data
+    static func testEggs() -> Recipe {
+    
+        let testSteps = [
+            "Add 1 teaspoon of oil to pan",
+            "Set heat to medium and wait for the pan to come to temperature",
+            "Crack egg, and drop gentlying into pan",
+            "Salt and pepper lightly",
+            "wait 1 1/2 minutes",
+            "Using spatula, flip the egg",
+            "wait an additional 1 1/2 minutes",
+            "Remove fried egg to plate"]
+    
+        return Recipe(title: "Fried Eggs", steps: testSteps)
+    }
+}
---
+++ SizzleTests/RecipeTests.swift
@@ -0,0 +1,52 @@
+//
+//  RecipeTests.swift
+//  SizzleTests
+//
+//  Created by Joshua Fredrickson on 11/26/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import XCTest
+@testable import Sizzle
+
+class RecipeTests: XCTestCase {
+    
+    var recipeUnderTest: Recipe!
+    let testSteps = [
+        "Add 1 teaspoon of oil to pan",
+        "Set heat to medium and wait for the pan to come to temperature",
+        "Crack egg, and drop gentlying into pan",
+        "Salt and pepper lightly",
+        "wait 1 1/2 minutes",
+        "Using spatula, flip the egg",
+        "wait an additional 1 1/2 minutes",
+        "Remove fried egg to plate"]
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        recipeUnderTest = Recipe(steps: testSteps)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testNext() {
+        // test that next funciton returns the correct values
+        for testStep in testSteps {
+            let nextStep = recipeUnderTest.next()
+            XCTAssertEqual(nextStep ?? "nil", testStep)
+        }
+        
+        // test that last step returns nil
+        XCTAssertNil(recipeUnderTest.next())
+        
+        // test empty list returns nil
+        let emptyRecipe = Recipe(steps: [])
+        XCTAssertNil(emptyRecipe.next())
+        
+    }
+    
+}
