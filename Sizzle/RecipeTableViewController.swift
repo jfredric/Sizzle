7982cdcde2606e75a2cc161c5cea8f1083970f73
jfredrickson <jfredric@gmail.com>
2017-11-29 17:07:36 -0800

    Tested text to speech by adding speech synthesis to didselectrowat function

--- Sizzle/RecipeTableViewController.swift
+++ Sizzle/RecipeTableViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecipeTableViewController: UITableViewController {
+    
+    let speechSynthesizer = AVSpeechSynthesizer()
     
     var currentRecipe: Recipe!
 
@@ -47,54 +50,13 @@ class RecipeTableViewController: UITableViewController {
         // Configure the cell...
         cell.titleLabel.text = currentRecipe.steps[indexPath.row]
         
-
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let speechText = currentRecipe.steps[indexPath.row]
+        let speechUtterance = AVSpeechUtterance(string: speechText)
+        speechSynthesizer.speak(speechUtterance)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
